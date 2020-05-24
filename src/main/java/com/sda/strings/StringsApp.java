d53dbc08e7f36507d5be4983767a7b850b6d5034
Uzytkownik <uzytkownik@kursGIT.pl>
2020-05-24 14:49:11 +0200

    stringsApp commit

--- src/main/java/com/sda/strings/StringsApp.java
+++ src/main/java/com/sda/strings/StringsApp.java
@@ -3,14 +3,14 @@ package com.sda.strings;
 public class StringsApp {
     public static void main(String[] args) {
         //TextUpperCase textUpperCase = new TextUpperCase();
-        String text = "Ala ma Kota";
+//        String text = "Ala ma Kota";
         //System.out.println("Before: " + text);
 
-        StringOperation operation = new TextUpperCase();
-        System.out.println(operation.modify(text));
-
-        operation = new TextDuplicate();
-        System.out.println(operation.modify(text));
+//        StringOperation operation = new TextUpperCase();
+//        System.out.println(operation.modify(text));
+//
+//        operation = new TextDuplicate();
+//        System.out.println(operation.modify(text));
 
         //text = textUpperCase.modify(text);
         //System.out.println("After: " + text);
@@ -19,6 +19,13 @@ public class StringsApp {
         //text = textDuplicate.modify(text);
         //System.out.println("After duplicate: " + text);
 
+        StringOperation appendTextOperationAnonClass = new StringOperation() {
+            @Override
+            public String modify(String text) {
+                return "Modified by anonymous class: " + text;
+            }
+        };
+        System.out.println(appendTextOperationAnonClass.modify("Karol"));
     }
 
     public static void  modifyAndPrint(String text, StringOperation operation){
